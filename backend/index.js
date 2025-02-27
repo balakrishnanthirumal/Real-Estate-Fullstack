eadd348ac9b1ef74ce8d8d7d2bebdaa831478e90
balakrishnanthirumal <balakrishnanthirumalt@gmail.com>
2025-02-27 08:38:57 +0530

    create signup api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+app.use(express.json());
+
+// app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
